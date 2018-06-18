eb7ca8e9961d99b4079b47726252886e6c7251c9
francolisotti <francolisotti96@gmail.com>
2018-06-18 12:54:35 -0300

    finished

--- src/main/java/com/utn/tp5/frontEnd/model/RouteXCabinFormated.java
+++ src/main/java/com/utn/tp5/frontEnd/model/RouteXCabinFormated.java
@@ -1,9 +1,13 @@
 package com.utn.tp5.frontEnd.model;
 
+import com.utn.tp5.DTO.PriceDTO;
 import com.utn.tp5.DTO.RouteXCabinDTO;
+import com.utn.tp5.model.Price;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,11 +16,14 @@ public class RouteXCabinFormated {
 
     private CabinFormated cabin;
     private RouteFormated route;
-    private PriceFormated price;
+    private List<PriceFormated> prices;
 
     public RouteXCabinFormated(RouteXCabinDTO routeXCabin) {
         this.cabin = new CabinFormated(routeXCabin.getCabin());
         this.route = new RouteFormated(routeXCabin.getRoute());
-        this.price = new PriceFormated(routeXCabin.getPrice());
+        this.prices = new ArrayList<>();
+        for (PriceDTO price: routeXCabin.getPrices()){
+            this.prices.add(new PriceFormated(price));
+        }
     }
 }
